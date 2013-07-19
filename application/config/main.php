7df68e0a939ea4975fae506ac8863bf40c4d2348
Paris Theofanidis <ptheofan@gmail.com>
2013-07-20 01:45:17 +0200

    local config

--- application/config/main.php
+++ application/config/main.php
@@ -2,9 +2,6 @@
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
-
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Git Deployer',
@@ -32,11 +29,9 @@ return array(
 	// application components
 	'components'=>array(
 		'user'=>array(
-			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
 
-		// uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
             'urlFormat' => 'path',
             'showScriptName' => false,
@@ -48,7 +43,6 @@ return array(
 			),
 		),
 
-		// uncomment the following to use a MySQL database
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=deployer',
 			'emulatePrepare' => true,
